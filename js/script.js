4cfb984dcadd5927ec3f56c0b943256a4780aa6e
Gianluca Benassi <benox1199@gmail.com>
2021-11-09 14:12:08 +0100

    html style

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ const numbers = document.getElementById("numbers");
 
 for (let i = 1; i <= 100; i++) {
 
-    let text;
+    let text, liClass;
 
     // check multiples of 3 and 5
     if (i % 3 == 0 && i % 5 == 0) {
         text = "fizzbuzz";
+        liClass = "red";
     } else if (i % 3 == 0) {
         text = "fizz";
+        liClass = "green";
     } else if (i % 5 == 0) {
         text = "buzz";
+        liClass = "yellow";
     } else {
         text = i;
+        liClass = "blue";
     }
 
     // add number element to ul
-    numbers.innerHTML += `<li>${text}</li>`;
+    numbers.innerHTML += `<li class="box ${liClass}">${text}</li>`;
 
 }
